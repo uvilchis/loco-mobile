762289a5b453b41ff29753128f658424aa7b0d26
Christine Kong <ckong1792@gmail.com>
2017-12-06 12:26:23 -0500

    added lines

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcome to loco, your one stop resource for MTA delays</Text>
+        <Image source={require("./images/123.png")} />
+        <Image source={require("./images/456.png")} />
+        <Image source={require("./images/7.png")} />
+        <Image source={require("./images/ACE.png")} />
+        <Image source={require("./images/BDFM.png")} />
+        <Image source={require("./images/G.png")} />
+        <Image source={require("./images/JZ.png")} />
+        <Image source={require("./images/L.png")} />
+        <Image source={require("./images/NQR.png")} />
+        <Image source={require("./images/S.png")} />
       </View>
     );
   }
@@ -17,5 +28,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
