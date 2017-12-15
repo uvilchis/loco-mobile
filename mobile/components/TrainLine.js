3aa3ea013c85d7597572cd4c0b880b14a9633e3d
Samuel Kwak <kwak123@gmail.com>
2017-12-15 18:09:13 -0500

    small code cleanup

--- mobile/components/TrainLine.js
+++ mobile/components/TrainLine.js
@@ -6,27 +6,6 @@ import StatusMarker from './StatusMarker';
 
 import Helpers from '../lib/util';
 
-const markers = {
-  "GOOD SERVICE": require('../images/markers/green.png'),
-  "SERVICE CHANGE": require('../images/markers/red.png'),
-  "PLANNED WORK": require('../images/markers/yellow.png'),
-  "DELAYS": require('../images/markers/yellow.png')
-};
-
-const lines = {
-  "123": require('../images/lines/123.png'),
-  "456": require('../images/lines/456.png'),
-  "7": require('../images/lines/7.png'),
-  "ACE": require('../images/lines/ACE.png'),
-  "BDFM": require('../images/lines/BDFM.png'),
-  "G": require('../images/lines/G.png'),
-  "JZ": require('../images/lines/JZ.png'),
-  "L": require('../images/lines/L.png'),
-  "NQR": require('../images/lines/NQR.png'),
-  "S": require('../images/lines/S.png'),
-  "SIR": require('../images/lines/SIR.png')
-};
-
 export default class TrainLine extends Component {
   constructor(props) {
     super(props);
