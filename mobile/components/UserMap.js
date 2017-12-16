576cb0816d6846fdae3c83882b1defc424f2a1b0
Christine Kong <ckong1792@gmail.com>
2017-12-15 23:19:49 -0500

    added markers to user map

--- mobile/components/UserMap.js
+++ mobile/components/UserMap.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Constants, Location, Permissions, MapView } from 'expo';
+import axios from 'axios';
+import geodist from 'geodist';
 
 const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 };
 
@@ -7,34 +9,13 @@ export default class UserMap extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      location: { coords: {latitude: 0, longitude: 0}},
-      results: [],
-      dist: 0.5
+      location: [],
+      results: []
     };
   }
-  
+
   componentWillMount() {
     this.locationChanged()
-    .then((response) => {
-      console.log(response)
-      return axios.get(`http://10.16.1.191:3000/api/stops/coords`, {
-        sub: 'mta',
-        lat: this.state.location.coords.latitude,
-        long: this.state.location.coords.longitude,
-        dist: this.state.dist
-      })
-      .then((response) => {
-        this.setState({
-          results: response.data
-        })
-      })
-      .catch((err) => {
-        console.error(err)
-      })
-    })
-    .catch((err) => {
-      console.error(err)
-    })  
   }
 
   locationChanged = async () => {
@@ -44,12 +25,25 @@ export default class UserMap extends Component {
         errorMessage: 'Permission to access location was denied',
       });
     } 
-    let location = await Location.getCurrentPositionAsync({});
-    console.log(location)
+    
+    let location = await Location.getCurrentPositionAsync({})
+    this.setState({
+      location: location
+    }, () => {
+      axios.get(`http://10.16.1.191:3000/api/stops/location?lat=${this.state.location.coords.latitude}&lon=${this.state.location.coords.longitude}`)
+      .then((response) => {
+        this.setState({
+          results: response.data
+        })
+      })
+      .catch((err) => {
+        console.error('ERROR IN AXIOS REQUEST', err)
+      })
+    })
   }
 
   render() {
-    console.log('THIS.STATE.LOCATION IN USERMAP:', this.state.location)
+    console.log('RESULTS', this.state.results)
     return (
       <MapView
         style={{ flex: 1 }}
@@ -59,8 +53,17 @@ export default class UserMap extends Component {
           latitudeDelta: 0.01,
           longitudeDelta: 0.05,
         }}
-      />
-    );
+      >
+        {this.state.results.map((marker, idx) => (
+          <MapView.Marker
+            coordinate={{latitude: Number(marker.stop_lat), longitude: Number(marker.stop_lon)}}
+            title={marker.stop_id}
+            description={marker.stop_name}
+            key={idx}
+          />
+        ))}        
+      </MapView>
+    )
   }
 }
 
